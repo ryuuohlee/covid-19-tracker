cd09004e111a6bbca71fe7cba104eef775053ef9
Jeffrey Lee <jglee1@live.com>
2021-05-04 22:48:14 -0700

    Feature: started Header

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>
+    <div> Covid 19 Tracker</div>
+    <p>API by covid19.mathro.id/api</p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
